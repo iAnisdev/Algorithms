efbfc271976c9b8f1c913040b660123c83e2bc30
Anis <me.anis.dev@gmail.com>
2024-10-14 23:20:01 +0100

    :zap: two sum in python

---
+++ python/twosum.py
@@ -0,0 +1,9 @@
+def twoSum(self, nums: List[int], target: int):
+        hash = {}
+        for i in range(len(nums)):
+            hash[nums[i]] = i
+        for i in range(len(nums)):
+              if target - nums[i] in hash:
+                if hash[target - nums[i]] != i:
+                  return [i ,hash[target - nums[i]] ]  
+        return []
