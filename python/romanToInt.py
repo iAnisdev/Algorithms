cff00ef9cf2fe4963fefd884359d88758ec3173a
Anis <me.anis.dev@gmail.com>
2024-10-15 21:34:42 +0100

    :zap: romanToInt

---
+++ python/romanToInt.py
@@ -0,0 +1,20 @@
+def romanToInt(self, s: str) -> int:
+      actualValue = {
+          "I": 1,
+          "V": 5,
+          "X": 10,
+          "L": 50,
+          "C": 100,
+          "D": 500,
+          "M": 1000
+      }
+      sum = 0
+      for i, r in enumerate(s):
+          if i + 1 == len(s):
+              sum += actualValue[r]
+          else:
+              if actualValue[r] >= actualValue[s[i + 1]]:
+                  sum += actualValue[r]
+              else:
+                  sum -= actualValue[r]
+      return sum
