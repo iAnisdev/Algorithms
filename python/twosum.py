62f78ac034870da28bcb4bf0aac617d46139a86c
Anis <me.anis.dev@gmail.com>
2025-02-28 01:29:47 +0000

    :zap: add twoSumSorted function for handling sorted arrays

--- python/twosum.py
+++ python/twosum.py
@@ -10,3 +10,25 @@ def twoSum(nums: List[int], target: int):
             if hash[target - nums[i]] != i:
                 return [i, hash[target - nums[i]]]
     return []
+
+
+# incase array is sorted
+def twoSumSorted(numbers, target):
+    """
+    :type numbers: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        curr = numbers[left] + numbers[right]
+        if curr == target:
+            return [left + 1, right + 1]
+
+        if curr > target:
+            right = right - 1
+        else:
+            left = left + 1
+    return []
