eb417dc233439f7bf21835b613bc4a1467c52327
Anis <me.anis.dev@gmail.com>
2025-03-01 23:12:22 +0000

    :zap: optimize square_Arr function for improved readability and performance

--- python/squareArr.py
+++ python/squareArr.py
@@ -1,19 +1,18 @@
 def square_Arr(nums):
     if len(nums) < 1 or len(nums) > 10**4:
         raise ValueError("Out of range")
-    
-    result = [ i for i in range(len(nums))]
-    left , pos , right =  0 , len(nums) -1 , len(nums) -1
-    while left <= right:
+
+    result = [i for i in range(len(nums))]
+    left, right = 0, len(nums) - 1
+    for pos in range(len(nums) - 1, -1, -1):
         if abs(nums[left]) > abs(nums[right]):
-            result[pos] = nums[left]**2
-            pos = pos - 1
+            result[pos] = nums[left] ** 2
             left = left + 1
         else:
-            result[pos] = nums[right]**2
-            pos = pos - 1
+            result[pos] = nums[right] ** 2
             right = right - 1
     return result
 
-print(square_Arr([-4,-1,0,3,10])) #[0,1,9,16,100]
-print(square_Arr([-7,-3,2,3,11])) #[4,9,9,49,121] 
+
+print(square_Arr([-4, -1, 0, 3, 10]))  # [0,1,9,16,100]
+print(square_Arr([-7, -3, 2, 3, 11]))  # [4,9,9,49,121]
